770dc49799de5ad3a496e14e5e0aa003b23900a9
PC <jupallyyashwanth376@gmail.com>
2024-10-17 12:55:46 +0530

    commit

--- src/main/java/com/oauth/sample/SecurityConfig.java
+++ src/main/java/com/oauth/sample/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                         authorizeRequests.anyRequest().authenticated())
 //                .formLogin(form -> form.defaultSuccessUrl("/hello", true));
                 .oauth2Login(oauth2 ->
-                        oauth2.defaultSuccessUrl("/hello", true));
+                        oauth2.defaultSuccessUrl("http://localhost:3000/dashboard", true));
         return http.build();
     }
 
